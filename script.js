f7248f2bb925c89cccb581b4959a92e01015a06c
KhangNguyen872 <147101111+KhangNguyen872@users.noreply.github.com>
2024-02-08 00:03:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+//Creating a parallax effect,
+//Grabbing Id from css/html and then assgning it to a variable. Then adjust each variable y value by wacthing scroll.
 document.addEventListener('DOMContentLoaded', function () {
     let sky = document.getElementById('sky');
     let mainComputer = document.getElementById('mainComputer');
@@ -16,6 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//Creating a searchbar,
+//Grabbing id and tag name from html/css and then filtering out rows by hiding rows that doesn't match search query.
+//This is achieved by a for loop that checks over eahc tr/row, then selects td and store it inside txtValue.
+//Then check if txtValue matches the searchfilter/seqrch quries. Then proceed to show/hide the rows if it amtches or not.
 function searchBar() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
@@ -35,6 +40,9 @@ function searchBar() {
     }
 }
 
+//Creating a dropdown button,
+//Toggles show and hide by using a window event listen, listening if the user click on the class dropbtn.
+//If event matches dropbtn, then show dropdown.
 function dropbtn() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -52,6 +60,10 @@ window.onclick = function(event) {
   }
 }
 
+//Creates a filter elements,
+//First check if the document is fully loaded then calls filterSelection(all), this display all the row.
+//Takes a parameter of c then selects all row through the class filterRow within the id myTable. Then check if the row equal to c, if yes then shows all.
+//Otherwise, the row is hidden.
 document.addEventListener("DOMContentLoaded", function () {
   filterSelection("all");
 });
@@ -68,6 +80,9 @@ function filterSelection(c) {
   });
 }
 
+//Uses a click event listner that if a button within the dropdown or class myBtnContainer is clicked.
+// Then remove any pre-existing active class then reassign the active class to the clicked button.
+//Checks the content of button, like text. Trim any spaces. Then send it to filterSelection variable/parameter c.
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByTagName("button");
 
@@ -79,6 +94,6 @@ for (var i = 0; i < btns.length; i++) {
       this.className += " active";
 
       var country = this.textContent.trim();
-      filterSelection(country);
+      filterSelection(c);
   });
 }
